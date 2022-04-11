627b3db2ff3328f8d12d814f8f9ca19d9bc81ca1
Alessandro Lima <alessandroslima@hotmail.com>
2022-04-11 17:52:23 -0300

    Base project

---
+++ src/main/java/br/com/alima/alurachallenge3/AluraChallenge3Application.java
@@ -0,0 +1,13 @@
+package br.com.alima.alurachallenge3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluraChallenge3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluraChallenge3Application.class, args);
+	}
+
+}
